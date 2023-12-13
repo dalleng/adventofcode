ac7aff47b11df22600c392e0d9e4acd174cd865f
Diego Allen <diegoallen@gmail.com>
2023-12-12 22:04:15 -0300

    Add python solution to day 3 part 2

--- 2023/day3/main.py
+++ 2023/day3/main.py
@@ -1,5 +1,6 @@
 import re
 from typing import NamedTuple
+from collections import defaultdict
 
 
 class PartNumber(NamedTuple):
@@ -12,78 +13,121 @@ class PartNumber(NamedTuple):
 def main():
     with open("input.txt") as f:
         schematic = [line.strip() for line in f]
-        # print(schematic)
 
-    s = 0
+    s1 = 0
+    adjacent_asterisks = defaultdict(list)
+
     for part in find_parts(schematic):
-        print(f"{part=}")
-        if is_valid(schematic, part):
-            print("is valid")
-            s += part.number
-        else:
-            print("is not valid")
+        if is_valid(schematic, part, adjacent_asterisks):
+            s1 += part.number
 
-    print(f"{s=}")
+    print(f"{s1=}")
+
+    s2 = 0
+    for _, value in adjacent_asterisks.items():
+        if len(value) == 2:
+            s2 += value[0] * value[1]
+
+    print(f"{s2=}")
 
 
 def find_parts(schematic):
     for row, line in enumerate(schematic):
         for m in re.finditer(r"\d+", line):
             start, end = m.span()
-            yield PartNumber(number=int(m.group()), row=row, start=start, end=end-1)
+            yield PartNumber(
+                number=int(m.group()),
+                row=row,
+                start=start,
+                end=end-1
+            )
 
 
-def is_valid(schematic, part):
+def is_valid(schematic, part, adjacent_asterisks):
+    is_valid = False
+
     # check one position below start
-    if part.start > 0 and is_symbol(schematic[part.row][part.start - 1]):
-        return True
+    row = part.row
+    col = part.start - 1
+    if part.start > 0 and is_symbol(schematic[row][col]):
+        if schematic[row][col] == '*':
+            adjacent_asterisks[(row, col)].append(part.number)
+        is_valid = True
 
     # check the upper diagonal of the start
+    row = part.row - 1
+    col = part.start - 1
     if (
         part.start > 0
         and part.row > 0
-        and is_symbol(schematic[part.row - 1][part.start - 1])
+        and is_symbol(schematic[row][col])
     ):
-        return True
+        if schematic[row][col] == '*':
+            adjacent_asterisks[(row, col)].append(part.number)
+        is_valid = True
 
     # check the lower diagonal of the start
+    row = part.row + 1
+    col = part.start - 1
     if (
         part.start > 0
         and part.row < len(schematic) - 1
-        and is_symbol(schematic[part.row + 1][part.start - 1])
+        and is_symbol(schematic[row][col])
     ):
-        return True
+        if schematic[row][col] == '*':
+            adjacent_asterisks[(row, col)].append(part.number)
+        is_valid = True
 
+    row = part.row
+    col = part.end + 1
     # check one postion after the end
     if part.end < len(schematic[0]) - 1 and is_symbol(
-        schematic[part.row][part.end + 1]
+        schematic[row][col]
     ):
-        return True
+        if schematic[row][col] == '*':
+            adjacent_asterisks[(row, col)].append(part.number)
+        is_valid = True
 
     # check upper diagonal of the end
+    row = part.row - 1
+    col = part.end + 1
     if (
         part.end < len(schematic[0]) - 1
         and part.row > 0
-        and is_symbol(schematic[part.row - 1][part.end + 1])
+        and is_symbol(schematic[row][col])
     ):
-        return True
+        if schematic[row][col] == '*':
+            adjacent_asterisks[(row, col)].append(part.number)
+        is_valid = True
 
     # check the lower diagonal of the end
+    row = part.row + 1
+    col = part.end + 1
     if (
         part.end < len(schematic[0]) - 1
         and part.row < len(schematic) - 1
-        and is_symbol(schematic[part.row + 1][part.end + 1])
+        and is_symbol(schematic[row][col])
     ):
-        return True
+        if schematic[row][col] == '*':
+            adjacent_asterisks[(row, col)].append(part.number)
+        is_valid = True
 
     for i in range(part.start, part.end+1):
         # check above
-        if part.row > 0 and is_symbol(schematic[part.row - 1][i]):
-            return True
+        row = part.row - 1
+        if part.row > 0 and is_symbol(schematic[row][i]):
+            is_valid = True
+            if schematic[row][i] == '*':
+                adjacent_asterisks[(row, i)].append(part.number)
 
         # check below
-        if part.row < len(schematic) - 1 and is_symbol(schematic[part.row + 1][i]):
-            return True
+        row = part.row + 1
+        if part.row < len(schematic) - 1 and is_symbol(schematic[row][i]):
+            is_valid = True
+            if schematic[row][i] == '*':
+                adjacent_asterisks[(row, i)].append(part.number)
+
+    return is_valid, adjacent_asterisks
 
 
 def is_symbol(c):
