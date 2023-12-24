9a4776ab285326c29bfad1ed333a88fa3a54287c
Diego Allen <diegoallen@gmail.com>
2023-12-23 23:36:09 -0300

    Add python solution for day 4 part 2

--- 2023/day4/main.py
+++ 2023/day4/main.py
@@ -1,14 +1,32 @@
 import re
+from collections import Counter
 
 
 def main():
     s = 0
+    s2 = 0
+    counter = Counter()
+
     with open('input.txt') as file:
-        for line in file:
-            winning_numbers, own_numbers = extract_numbers(line)
-            intersection = set(winning_numbers).intersection(set(own_numbers))
-            s += 2 ** (len(intersection)-1) if intersection else 0
+        cards = file.read().split('\n')
+
+    for i, line in enumerate(cards, start=1):
+        counter[i] += 1
+        print(f"card {i} has {counter[i]} copies")
+
+        winning_numbers, own_numbers = extract_numbers(line)
+
+        intersection = set(winning_numbers).intersection(set(own_numbers))
+        matching_cards = len(intersection)
+
+        for j in range(1, matching_cards+1):
+            counter[i+j] += 1 * counter[i]
+
+        s += 2 ** (matching_cards-1) if intersection else 0
+
+    s2 = sum(counter.values())
     print(f"{s=}")
+    print(f"{s2=}")
 
 
 def extract_numbers(line):
