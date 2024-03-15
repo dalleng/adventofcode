3123f55156ca382768cc29a4e3da0099438ee9dc
Diego Allen <diegoallen@gmail.com>
2024-03-15 12:14:51 -0300

    Add solution to day 6 part 1 in python

---
+++ 2023/day6/main.py
@@ -0,0 +1,50 @@
+import re
+import math
+
+
+def main():
+    with open("input.txt") as f:
+        times = next(f)
+        distances = next(f)
+        times = list(map(int, re.findall(r"\d+", times)))
+        distances = list(map(int, re.findall(r"\d+", distances)))
+
+    s1 = 1
+    for time, distance in zip(times, distances):
+        s1 *= number_of_ways_to_beat(time, distance)
+    print(f"{s1=}")
+
+
+def number_of_ways_to_beat(time, distance):
+    speed_to_beat = calculate_speed(time, distance)
+    start = math.floor(speed_to_beat+1)
+    ways = 0
+
+    for i in range(start, time // 2 + 1):
+        current_distance = (time - i) * i
+        if current_distance > distance:
+            if time - i != i:
+                ways += 2
+            else:
+                ways += 1
+        else:
+            break
+
+    return ways
+
+
+def calculate_speed(time, distance):
+    """Calculates the speed to have achieved that distance
+    considering the time constraint."""
+    # base on quadratic equation (-b +- sqrt(b**2-4ac)) / 2
+    solution = (time - math.sqrt(time**2-4*distance)) / 2
+    return solution
+
+
+if __name__ == '__main__':
+    main()
+
+    assert number_of_ways_to_beat(7, 9) == 4
+    assert number_of_ways_to_beat(15, 40) == 8
+    assert number_of_ways_to_beat(30, 200) == 9
+    assert number_of_ways_to_beat(71530, 940200) == 71503
