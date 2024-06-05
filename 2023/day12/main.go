a9d398a834037a93109618ebdc8acecd3e291357
Diego Allen <diegoallen@gmail.com>
2024-06-05 17:38:15 -0400

    Add solution to day 12 part 2 in go

--- 2023/day12/main.go
+++ 2023/day12/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -17,11 +18,13 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 	s1 := 0
+	s2 := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		springsAndCounts := strings.Split(line, " ")
 		counts := make([]int, 0)
+
 		for _, n := range strings.Split(springsAndCounts[1], ",") {
 			if value, err := strconv.Atoi(n); err != nil {
 				log.Fatal(err)
@@ -29,44 +32,79 @@ func main() {
 				counts = append(counts, value)
 			}
 		}
-		s1 += count(springsAndCounts[0], counts)
-	}
 
-	log.Printf("s1: %d", s1)
+		s1 += count(springsAndCounts[0], counts)
+
+		// Unfold and call count again
+		countsUnfolded := make([]int, 0, len(counts)*5)
+		springsUnfolded := ""
+
+		for i := 0; i < 5; i++ {
+			springsUnfolded += springsAndCounts[0]
+			if i < 4 {
+				springsUnfolded += "?"
+			}
+			for _, n := range counts {
+				countsUnfolded = append(countsUnfolded, n)
+			}
+		}
+
+		s2 += count(springsUnfolded, countsUnfolded)
+	}
 }
 
 func count(springs string, counts []int) int {
-	if springs == "" {
-		if len(counts) == 0 {
-			return 1
-		} else {
-			return 0
+	cache := make(map[string]int)
+
+	generateCacheKey := func(springs string, counts []int) string {
+		countStr := ""
+		for _, n := range counts {
+			countStr += fmt.Sprintf("%d", n)
 		}
+		return fmt.Sprintf("%s%s", springs, countStr)
 	}
 
-	if len(counts) == 0 {
-		if strings.Contains(springs, "#") {
-			return 0
-		} else {
-			return 1
+	var inner func(string, []int) int
+	inner = func(springs string, counts []int) int {
+		key := generateCacheKey(springs, counts)
+		if value, ok := cache[key]; ok {
+			return value
 		}
-	}
-
-	result := 0
-
-	if current := []rune(springs)[0]; current == '.' || current == '?' {
-		result += count(string([]rune(springs)[1:]), counts)
-	}
-
-	if current := []rune(springs)[0]; current == '#' || current == '?' {
-		if counts[0] <= len([]rune(springs)) && !strings.Contains(string([]rune(springs)[:counts[0]]), ".") && (len([]rune(springs)) == counts[0] || []rune(springs)[counts[0]] != '#') {
-			springsLeft := ""
-			if counts[0]+1 < len([]rune(springs)) {
-				springsLeft = string([]rune(springs)[counts[0]+1:])
+		if springs == "" {
+			if len(counts) == 0 {
+				return 1
+			} else {
+				return 0
 			}
-			result += count(springsLeft, counts[1:])
 		}
+
+		if len(counts) == 0 {
+			if strings.Contains(springs, "#") {
+				return 0
+			} else {
+				return 1
+			}
+		}
+
+		result := 0
+
+		if current := []rune(springs)[0]; current == '.' || current == '?' {
+			result += inner(string([]rune(springs)[1:]), counts)
+		}
+
+		if current := []rune(springs)[0]; current == '#' || current == '?' {
+			if counts[0] <= len([]rune(springs)) && !strings.Contains(string([]rune(springs)[:counts[0]]), ".") && (len([]rune(springs)) == counts[0] || []rune(springs)[counts[0]] != '#') {
+				springsLeft := ""
+				if counts[0]+1 < len([]rune(springs)) {
+					springsLeft = string([]rune(springs)[counts[0]+1:])
+				}
+				result += inner(springsLeft, counts[1:])
+			}
+		}
+
+		cache[key] = result
+		return result
 	}
 
-	return result
+	return inner(springs, counts)
 }
