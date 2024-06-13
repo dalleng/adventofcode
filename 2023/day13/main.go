e6160d744f39e2a2101efc5a954d7c5fa89e30ea
Diego Allen <diegoallen@gmail.com>
2024-06-13 11:41:27 -0400

    Add solution to day 13 part 2 in go

--- 2023/day13/main.go
+++ 2023/day13/main.go
@@ -36,11 +36,17 @@ func main() {
 	}
 
 	s1 := 0
+	s2 := 0
+
 	for _, pattern := range patterns {
 		s1 += findColReflection(pattern)
+		s2 += findColReflectionWithSmudge(pattern)
 		s1 += 100 * findRowReflection(pattern)
+		s2 += 100 * findRowReflectionWithSmudge(pattern)
 	}
+
 	log.Printf("s1=%d", s1)
+	log.Printf("s2=%d", s2)
 }
 
 func transpose(s []string) []string {
@@ -53,6 +59,21 @@ func transpose(s []string) []string {
 		transposed = append(transposed, line)
 	}
 	return transposed
+}
+
+func isEqualWithDiffTolerance(s1 string, s2 string, allowedDiff int) (bool, []int) {
+	s1AsRunes := []rune(s1)
+	s2AsRunes := []rune(s2)
+	diffCount := 0
+	diffIndices := []int{}
+	for i := 0; i < utf8.RuneCountInString(s1); i++ {
+		if s1AsRunes[i] != s2AsRunes[i] {
+			diffCount += 1
+			diffIndices = append(diffIndices, i)
+		}
+	}
+	equal := diffCount <= allowedDiff
+	return equal, diffIndices
 }
 
 func findRowReflection(pattern []string) int {
@@ -81,6 +102,50 @@ func findRowReflection(pattern []string) int {
 	}
 
 	return 0
+}
+
+func findRowReflectionWithSmudge(pattern []string) int {
+	reflectionFound := false
+
+	for i := 0; i < len(pattern)-1; i++ {
+		isEqualWithTolerance, indices := isEqualWithDiffTolerance(pattern[i], pattern[i+1], 1)
+		smudgeFound := len(indices) > 0
+		if isEqualWithTolerance {
+			reflectionFound = true
+			reflectionSize := 0
+
+			if reflectionSizeRight := len(pattern) - (i + 2); i < reflectionSizeRight {
+				reflectionSize = i
+			} else {
+				reflectionSize = reflectionSizeRight
+			}
+
+			for j := 1; j <= reflectionSize; j++ {
+				diffTolerance := 0
+				if !smudgeFound {
+					diffTolerance = 1
+				}
+				isEqualWithTolerance, indices := isEqualWithDiffTolerance(pattern[i-j], pattern[i+1+j], diffTolerance)
+				if !smudgeFound {
+					smudgeFound = len(indices) > 0
+				}
+				if !isEqualWithTolerance {
+					reflectionFound = false
+				}
+			}
+
+			if reflectionFound && smudgeFound {
+				return i + 1
+			}
+		}
+	}
+
+	return 0
+}
+
+func findColReflectionWithSmudge(pattern []string) int {
+	transposed := transpose(pattern)
+	return findRowReflectionWithSmudge(transposed)
 }
 
 func findColReflection(pattern []string) int {
