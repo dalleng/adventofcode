06133f5e01193441f1f9c6157769605ffa41468c
Diego Allen <diegoallen@gmail.com>
2024-07-08 13:09:09 -0400

    Add solution to day 14 part 2 in go

--- 2023/day14/main.go
+++ 2023/day14/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"hash/fnv"
 	"log"
 	"os"
 )
 
 func main() {
 	f, err := os.Open("input.txt")
+	// f, err := os.Open("example.txt")
 	defer f.Close()
 	if err != nil {
 		log.Fatal(err)
@@ -28,27 +30,90 @@ func main() {
 		lineno++
 	}
 
-	for _, runes := range platform {
-		fmt.Println(string(runes))
+	copy := copyRuneSlice(platform)
+	tiltVertical(copy, "north")
+	fmt.Printf("s1=%d\n", calculateLoad(copy))
+
+	cycleSize := 0
+	cycleCount := 0
+	cache := map[uint64]int{}
+	for true {
+		key := getCacheKey(platform)
+		if value, ok := cache[key]; ok {
+			cycleSize = cycleCount - value
+			break
+		} else {
+			cache[key] = cycleCount
+		}
+		cycle(platform)
+		cycleCount++
 	}
 
-	tilt(platform)
-	fmt.Println()
-
-	for _, runes := range platform {
-		fmt.Println(string(runes))
+	remaining := (1000_000_000 - cycleCount) % cycleSize
+	if remaining > 0 {
+		for i := 0; i < remaining; i++ {
+			cycle(platform)
+		}
 	}
 
-	fmt.Printf("s1=%d\n", calculateLoad(platform))
+	fmt.Printf("s2=%d\n", calculateLoad(platform))
 }
 
-func tilt(platform [][]rune) {
+func printPlatform(platform [][]rune) {
+	for _, runes := range platform {
+		fmt.Println(string(runes))
+	}
+}
+
+func getCacheKey(platform [][]rune) uint64 {
+	h := fnv.New64a()
+	for _, row := range platform {
+		for _, r := range row {
+			// Convert rune to bytes and write to the hash function
+			var buf [4]byte
+			n := copy(buf[:], string(r))
+			h.Write(buf[:n])
+		}
+	}
+	return h.Sum64()
+}
+
+func copyRuneSlice(original [][]rune) [][]rune {
+	copied := make([][]rune, len(original))
+
+	for i, innerSlice := range original {
+		copiedInnerSlice := make([]rune, len(innerSlice))
+		copy(copiedInnerSlice, innerSlice)
+		copied[i] = copiedInnerSlice
+	}
+
+	return copied
+}
+
+func cycle(platform [][]rune) {
+	for _, direction := range []string{"north", "west", "south", "east"} {
+		if direction == "north" || direction == "south" {
+			tiltVertical(platform, direction)
+		}
+		if direction == "east" || direction == "west" {
+			tiltHorizontal(platform, direction)
+		}
+	}
+}
+
+func tiltVertical(platform [][]rune, direction string) {
 	for j := 0; j < len(platform[0]); j++ {
 		freePosition := -1
 		for i := 0; i < len(platform); i++ {
-			current := platform[i][j]
+			var row int
+			if direction == "north" {
+				row = i
+			} else {
+				row = len(platform) - 1 - i
+			}
+			current := platform[row][j]
 			if current == '.' && freePosition == -1 {
-				freePosition = i
+				freePosition = row
 			}
 			// take into account # that blocks sliding
 			if current == '#' {
@@ -56,8 +121,43 @@ func tilt(platform [][]rune) {
 			}
 			if current == 'O' && freePosition != -1 {
 				platform[freePosition][j] = 'O'
-				platform[i][j] = '.'
-				freePosition++
+				platform[row][j] = '.'
+				if direction == "north" {
+					freePosition++
+				} else {
+					freePosition--
+				}
+			}
+		}
+	}
+}
+
+func tiltHorizontal(platform [][]rune, direction string) {
+	for i := 0; i < len(platform); i++ {
+		freePosition := -1
+		for j := 0; j < len(platform[0]); j++ {
+			var col int
+			if direction == "west" {
+				col = j
+			} else {
+				col = len(platform[0]) - 1 - j
+			}
+			current := platform[i][col]
+			if current == '.' && freePosition == -1 {
+				freePosition = col
+			}
+			// take into account # that blocks sliding
+			if current == '#' {
+				freePosition = -1
+			}
+			if current == 'O' && freePosition != -1 {
+				platform[i][freePosition] = 'O'
+				platform[i][col] = '.'
+				if direction == "west" {
+					freePosition++
+				} else {
+					freePosition--
+				}
 			}
 		}
 	}
